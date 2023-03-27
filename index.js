a6c34f5bad94c60044543ad9f0fed1de53fe8c83
Yaser Eisa <yasereisa@Yasers-MacBook-Air.local>
2023-03-27 19:55:06 -0400

    pull

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function getRoomsByUser(id){
 app.use(express.json());
 const io = new Server(server, {
   cors :{
-    origin : ['http://localhost:1234','http://localhost:3000'],
+    origin : ['http://localhost:1234'],
     methods:['GET','POST','PUT']
   }
 })
